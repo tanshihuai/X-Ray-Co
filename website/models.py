611af705562b88c8e4eb5c1471183c1886665f4a
shihuai <tanshihuai@gmail.com>
2022-08-09 04:57:54 +0800

    models (not migrated)

--- website/models.py
+++ website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Patient(models.Model):
+    NRIC = models.CharField(max_length=9)
+    Name = models.CharField(max_length=100)
+    Phone = models.IntegerField(unique=True)
+    Email = models.EmailField(unique=True)
